1499e5897e2170190377685fb9742e34fb007a87
Geromiel <75524331+Geromiel@users.noreply.github.com>
2023-04-06 11:33:14 +0200

    Implemented try catch statement to avoid errors during nighttime. Changed variable names to match config file.

--- Solaranlage.py
+++ Solaranlage.py
@@ -1,19 +1,21 @@
 import requests
 import re
 from influxdb import InfluxDBClient
+import config
 
 PATTERN = r'var webdata_now_p = "(.*?)";'
-HOSTNAME = 'IP OF SOLAR PANEL'
-INFLUXDB_IP = 'IP OF INFLUXDB'
-INFLUXDB_PORT = 'PORT OF INFLUXDB'
-USERNAME_DEYE = 'admin'
-PASSWORD_DEYE = 'admin'
+DEYE_IP = config.solarpanel['SOLARPANEL_IP']
+DEYE_USERNAME = config.solarpanel['DEYE_USERNAME']
+DEYE_PASSWORD = config.solarpanel['DEYE_PASSWORD']
+INFLUXDB_IP = config.database['INFLUXDB_IP']
+INFLUXDB_PORT = config.database['INFLUXDB_PORT']
+INFLUXDB_DATABASE_NAME = config.database['INFLUXDB_DATABASE_NAME']
 
 
 def website():
     session = requests.Session()
-    session.auth = (USERNAME_DEYE, PASSWORD_DEYE)
-    response = session.get('http://' + HOSTNAME + '/status.html')
+    session.auth = (DEYE_USERNAME, DEYE_PASSWORD)
+    response = session.get('http://' + DEYE_IP + '/status.html')
 
     html = response.text
 
@@ -27,7 +29,7 @@ def influxdb(power_output):
     power_output = power_output
     field = 'watt'
     client = InfluxDBClient(host=INFLUXDB_IP, port=INFLUXDB_PORT)
-    client.switch_database('solarpanel')
+    client.switch_database(INFLUXDB_DATABASE_NAME)
 
     data_point = [
         {
@@ -35,12 +37,15 @@ def influxdb(power_output):
             'fields': {field: power_output}
         }
     ]
-
     client.write_points(data_point)
 
 
 def main():
-    power_output = website()
+    try:
+        power_output = website()
+    except:
+        power_output = '0'
+        # Could implement alarming here, but my solar panel is not reachable during night time, so wouldn't be useful
     influxdb(power_output)
 
 
