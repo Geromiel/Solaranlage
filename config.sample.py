cb0ae6091c54cbc39ef12a43ee9e07bb42a85fe5
Geromiel <75524331+Geromiel@users.noreply.github.com>
2023-04-06 11:32:15 +0200

    Implemented config file to avoid hard coded config

---
+++ config.sample.py
@@ -0,0 +1,10 @@
+database = dict(
+    INFLUXDB_IP='###',
+    INFLUXDB_PORT='###',
+    INFLUXDB_DATABASE_NAME='###'
+)
+solarpanel = dict(
+    SOLARPANEL_IP='###',
+    USERNAME_DEYE='###',
+    PASSWORD_DEYE='###'
+)
